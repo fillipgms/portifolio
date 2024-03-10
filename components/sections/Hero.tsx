3040bf1c1da58f3d7e598761b1d4075d2ffc5b6f
Fillip Mangia <fillipgms@gmail.com>
2024-03-10 18:06:35 -0300

    responsivity ajust

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -5,23 +5,23 @@ import { Button } from "../ui/button";
 
 const Hero = () => {
     return (
-        <section className="h-svh grid grid-cols-2 py-5 px-10 gap-10 grid-rows-2">
+        <section className="h-svh grid md:grid-cols-2 grid-cols-1 py-5 px-10 gap-10 md:grid-rows-2">
             <Image
                 {...header}
                 alt="header"
-                className="col-span-2 h-full object-cover rounded-3xl"
+                className="md:col-span-2 h-full object-cover rounded-3xl"
             />
-            <div className="flex flex-col justify-center gap-5">
+            <div className="flex flex-col justify-center gap-5 ">
                 <Button
                     className="bg-[#7AC1DE] rounded-full px-10 text-lg w-1/2"
                     size={"lg"}
                 >
                     vamos conversar
                 </Button>
-                <h1 className="text-[5rem] font-bold">
+                <h1 className="text-[3.5rem] font-bold">
                     Fillip
                     <div className="flex items-center gap-2">
-                        <span className=" inline-block h-[5px] w-24 bg-black"></span>
+                        <span className=" inline-block h-[5px] w-20 bg-black"></span>
                         Mangia
                     </div>
                 </h1>
@@ -33,9 +33,9 @@ const Hero = () => {
                     standard dummy text ever since the 1500s
                 </p>
                 <div className="grid grid-cols-2 flex-1 gap-3">
-                    <div className="bg-slate-400 row-span-2 rounded-3xl"></div>
-                    <div className="bg-slate-400 rounded-3xl"></div>
-                    <div className="bg-slate-400 rounded-3xl"></div>
+                    <div className="bg-slate-400 row-span-2 rounded-2xl"></div>
+                    <div className="bg-slate-400 rounded-2xl"></div>
+                    <div className="bg-slate-400 rounded-2xl"></div>
                 </div>
             </div>
         </section>
